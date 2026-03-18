ff601ae68c83f878d4dcc12ac3453a5c180835c7
Turner Price <168574817+turnzz@users.noreply.github.com>
2026-03-18 08:53:54 -0400

    for johny

--- day-of-service/src/App.tsx
+++ day-of-service/src/App.tsx
@@ -1,19 +1,31 @@
 import { useState } from 'react'
 import * as React from 'react'
-import { Input, InputGroup, Kbd, Image } from "@chakra-ui/react"
+import hseLogo from "./assets/hseLogo.png"
+import { Input, InputGroup, Kbd, Image, Text, Container, SegmentGroup } from "@chakra-ui/react"
 import { LuSearch } from "react-icons/lu"
 import './App.css'
 import data from "./data.json"
 
 function App() {
- console.log(data);
+  
+  const [value, setValue] = useState<string | null>("Any")
 
   return (
     <>
-    <Image rounded="md" src="" alt="HSE Logo" />
-      <InputGroup flex="1" startElement={<LuSearch />} endElement={<Kbd>⌘K</Kbd>}>
+    <Container style={{ flexDirection: 'row', padding: 30, alignItems: 'center', marginBottom: '20px',  display: 'flex', height: '80px' }}>
+    <Image rounded="md" style={{ width: '100px' }} src={hseLogo} alt="HSE Logo" />
+    <Text style={{ flex: 1 }} >HSE Day of Service App</Text>
+    </Container>
+    {/* <Text>Day of service is a day, where instead of doing service, students across hse schools participate in leisurely activities within HSE, also check out LinkHands and Community AI App (Yet to be named)</Text> */}
+    <Text> Description</Text>
+      <InputGroup flex="1" startElement={<LuSearch />} >
         <Input placeholder="Search contacts" />
       </InputGroup>
+      <SegmentGroup.Root value={value} onValueChange={(e) => setValue(e.value)}>
+      <SegmentGroup.Indicator />
+      <SegmentGroup.Items items={["Any", "1", "2", "3", "5", "6", "7"]} />
+    </SegmentGroup.Root>
+    
     </>
   )
 }

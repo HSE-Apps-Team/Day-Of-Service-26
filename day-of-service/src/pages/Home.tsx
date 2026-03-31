eaec9ca52ba7aa0975fd448a4e654e318bb06e96
RexfordTolle <tollerex000@hsestudents.org>
2026-03-31 11:28:34 -0400

    ui tweak

--- day-of-service/src/pages/Home.tsx
+++ day-of-service/src/pages/Home.tsx
@@ -16,24 +16,30 @@ export default function Home() {
         <Container style={{ marginBottom: 50, display: "flex", justifyContent: isNarrow ? "center" : "space-between", flexWrap: "wrap", flexDirection: isNarrow ? "column" : "row", alignItems: isNarrow ? "center" : "stretch" }}>
         
         
-        <Box mt="4" style={{width: isNarrow ? "100%" : "30%", maxWidth: "420px", borderWidth: 3, borderRadius: 5, padding: 10, height: "250px", borderColor: "#01539c"}}>
+        <Box mt="4" style={{width: isNarrow ? "100%" : "30%", maxWidth: "420px", borderWidth: 3, borderRadius: 5, padding: 10, paddingBottom: 50, paddingTop: 50, height: "250px", borderColor: "#01539c", display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "center"}}>
+            <Container>
             <Text fontWeight="bold" mb="2" mt="2"> High School </Text>
-            <Text fontSize={12} style={{marginTop: 10}}>For students in HSE High School</Text>
-            <Button mt="2" width="70%" backgroundColor="#01539c" colorScheme="blue" size="sm" style={{marginTop: 90}} onClick={() => changeScreen("highschool")}>
+            <Text fontSize={12} style={{marginTop: 10}}>Activities for all students in Hamilton Southeastern High School</Text>
+            </Container>
+            <Button mt="2" width="70%" backgroundColor="#01539c" colorScheme="blue" size="sm" style={{}} onClick={() => changeScreen("highschool")}>
                 Select
             </Button>
         </Box>
-        <Box mt="4" style={{width: isNarrow ? "100%" : "30%", maxWidth: "420px", borderWidth: 3, borderRadius: 5, padding: 10, height: "250px", borderColor: "#01539c"}}>
+        <Box mt="4" style={{width: isNarrow ? "100%" : "30%", maxWidth: "420px", borderWidth: 3, borderRadius: 5, padding: 10, paddingBottom: 50, paddingTop: 50, height: "250px", borderColor: "#01539c", display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "center"}}>
+            <Container>
             <Text fontWeight="bold" mb="2" mt="2"> Intermediate </Text>
             <Text fontSize={12} style={{marginTop: 10}}>For teachers of intermediate classes, these activities are not available for High school students.</Text>
-            <Button mt="2" width="70%" backgroundColor="#01539c" colorScheme="blue" size="sm" style={{marginTop: 90}} onClick={() => changeScreen("intermediate")}>
+            </Container>
+            <Button mt="2" width="70%" backgroundColor="#01539c" colorScheme="blue" size="sm" style={{}} onClick={() => changeScreen("intermediate")}>
                 Select
             </Button>
         </Box>
-        <Box mt="4" style={{width: isNarrow ? "100%" : "30%", maxWidth: "420px", borderWidth: 3, borderRadius: 5, padding: 10, height: "250px", borderColor: "#01539c"}}>
+        <Box mt="4" style={{width: isNarrow ? "100%" : "30%", maxWidth: "420px", borderWidth: 3, borderRadius: 5, padding: 10, paddingBottom: 50, paddingTop: 50, height: "250px", borderColor: "#01539c", display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "center"}}>
+            <Container>
             <Text fontWeight="bold" mb="2" mt="2"> Elementary </Text>
             <Text fontSize={12} style={{marginTop: 10}}>For teachers of elementary classes, these activities are not available for High school students.</Text>
-            <Button mt="2" width="70%" backgroundColor="#01539c" colorScheme="blue" size="sm" style={{marginTop: 90}} onClick={() => changeScreen("elementary")}>
+            </Container>
+            <Button mt="2" width="70%" backgroundColor="#01539c" colorScheme="blue" size="sm" style={{}} onClick={() => changeScreen("elementary")}>
                 Select
             </Button>
         </Box>

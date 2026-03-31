1388225427328fd3e70e04bd74b5129b3d61aa3e
Turner Price <168574817+turnzz@users.noreply.github.com>
2026-03-31 10:30:53 -0400

    NavBar mobile works

--- day-of-service/src/components/NavBar.tsx
+++ day-of-service/src/components/NavBar.tsx
@@ -2,26 +2,33 @@ import {
     Image,
     Container,
     Text,
+    useMediaQuery,
 } from "@chakra-ui/react"
 import hseLogo from "../assets/hseLogo.png"
 
 
-const Navbar = () => (
+const Navbar = () => {
+  // useMediaQuery returns an array of booleans; destructure the first value
+  const [isSmall] = useMediaQuery(["(max-width: 550px)"]);
+
+  return (
     <Container
-                style={{
-                  flexDirection: "row",
-                  padding: 30,
-                  alignItems: "center",
-                  marginBottom: "20px",
-                  marginTop: "20px",
-                  display: "flex",
-                  height: "80px",
-                }}
-              >
-                <Image rounded="md" style={{ width: "100px" }} src={hseLogo} alt="HSE Logo" />
-                <Text style={{ flex: 1, fontSize: 30, fontFamily: "Poppins, sans-serif", fontWeight: "bold", color: "#01539c" }}>HSE Day of Service Opportunities</Text>
-                <Text width="80px"></Text>
+      style={{
+        flexDirection: "row",
+        padding: 30,
+        alignItems: "center",
+        marginBottom: "20px",
+        marginTop: "20px",
+        display: "flex",
+        height: "80px",
+      }}
+    >
+      <Image rounded="md" style={{ width: "100px" }} src={hseLogo} alt="HSE Logo" />
+      {!isSmall && <Text style={{ flex: 1, fontSize: 30, fontFamily: "Poppins, sans-serif", fontWeight: "bold", color: "#01539c" }}>HSE Day of Service Opportunities</Text>}
+      {isSmall && <Text style={{ flex: 1, fontSize: 20, fontFamily: "Poppins, sans-serif", fontWeight: "bold", color: "#01539c" }}>HSE Day of Service Opportunities</Text>}
+      {!isSmall && <Text width="80px"></Text>}
     </Container>
-)
+  );
+};
 
 export default Navbar
